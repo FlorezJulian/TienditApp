9c3779e4f4dfbe354f380d9f2aabb88e9137a705
FlorezJulian <florezjulian87@gmail.com>
2021-11-04 13:53:44 -0500

    Creacion de actividad de bienvenida
    Creacion de bienvenida

---
+++ app/src/main/java/com/Grupo5/tienditapp/Bienvenida.java
@@ -0,0 +1,14 @@
+package com.Grupo5.tienditapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
